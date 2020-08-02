e0c40d9614d823744820112e93cf350cd98996c6
Laura Shireman <shireman@uw.edu>
2020-08-02 15:09:55 -0700

    Updated data for candidates and UW salaries.

--- R/data.R
+++ R/data.R
@@ -1,21 +1,38 @@
-#' King County contributions to candidates and political committees in 2018
+#' Contributions to King County, WA, 2020 political candidates
 #'
-#' A dataset containing the amount of money donated to candidates for political
-#' office in King County, WA in 2018. For a complete list of all variables,
-#' please see the data source.
+#' A dataset containing the amounts of money donated to candidates for political
+#' offices in King County, Washington, as of August 1, 2020. For a complete list of all
+#' variables and their descriptions, please see
+#' \url{https://www.pdc.wa.gov/browse/open-data/contributions-candidates-and-political-committees}.
 #'
-#' @format A data.frame with 39192 rows and 37 variables:
-#' \describe{
-#'   \item{filer_name}{Name of the candidate}
-#'   \item{office}{The office they're running for}
+#' @format A data.frame with 37 variables: \describe{
+#'   \item{filer_name}{Name of the candidate} \item{office}{The office they're
+#'   running for} \item{legislative_district}{The legislative district of that
+#'   office} \item{party}{The political party with which they're affiliated}
+#'   \item{amount}{The dollar amount of the contribution}
+#'   \item{contributor_name}{The name of the person who made the donation} ... }
+#' @source \url{https://data.wa.gov/Politics/Contributions-to-Candidates-and-Political-Committe/kv7h-kjye/data}. Accessed on Aug. 1, 2020.
+"Candidates"
+
+#' Contributions to candidates for the Washington State Legislature for the 2020
+#' election
+#'
+#' A dataset containing the amounts of money donated to candidates for the
+#' Washington State legislature from three counties in Western Washington and
+#' three counties in Eastern Washington as of August 1, 2020. For a complete
+#' list of all variables and their descriptions, please see
+#' \url{https://www.pdc.wa.gov/browse/open-data/contributions-candidates-and-political-committees}.
+#'
+#' @format A data.frame with 38 variables: \describe{ \item{filer_name}{Name of
+#'   the candidate} \item{office}{The office they're running for}
 #'   \item{legislative_district}{The legislative district of that office}
 #'   \item{party}{The political party with which they're affiliated}
 #'   \item{amount}{The dollar amount of the contribution}
-#'   \item{contributor_name}{The name of the person who made the donation}
-#'   ...
-#' }
-#' @source \url{http://www.pdc.wa.gov}
-"Candidates"
+#'   \item{contributor_name}{The name of the person who made the donation} ... }
+#' @source
+#' \url{https://data.wa.gov/Politics/Contributions-to-Candidates-and-Political-Committe/kv7h-kjye/data}.
+#' Accessed on Aug. 1, 2020.
+"WALeg"
 
 #' Example data for converting data.frames from long to wide format
 #'
@@ -123,15 +140,13 @@
 #'
 #' Example mass spec data for a run of the metformin assay
 #'
-#' @format A data.frame with 37 rows and 6 variables:
-#' \describe{
-#'   \item{SampleID}{Sample ID}
-#'   \item{MET.nominalmass}{Nominal mass of metformin in the sample (ng)}
-#'   \item{MET.area}{Metformin peak area (counts)}
+#' @format A data.frame with 37 rows and 6 variables: \describe{
+#'   \item{SampleID}{Sample ID} \item{MET.nominalmass}{Nominal mass of metformin
+#'   in the sample (ng)} \item{MET.area}{Metformin peak area (counts)}
 #'   \item{d6MET.area}{d6-metformin peak area (counts)}
 #'   \item{MET.peakarearatios}{Peak area ratio of MET/d6-MET}
-#'   \item{Weight.1overx}{Weights to use for a 1-over-x weighting scheme (1/MET nominal mass)}
-#' }
+#'   \item{Weight.1overx}{Weights to use for a 1-over-x weighting scheme (1/MET
+#'   nominal mass)} }
 "ExStdCurve"
 
 
@@ -155,29 +170,25 @@
 
 
 
-#' Salaries of University of Washington employees from 2013-2017 (public record)
+#' Salaries of University of Washington employees from 2014-2018 (public record)
 #'
-#' The pre-tax salaries of all UW employees from 2013 to 2017 in US dollars. (NB:
-#' Not all employees worked a full year or worked a full year in a given position.)
-#' Why am I including this in this package? Because it's a great data set and also because 1. you have a right to
-#' know as a taxpayer and 2. if you're a UW employee (and most of the people who will use this package are), you should know
-#' that this information is part of the public record. Same thing for your
-#' UW email, fyi. If you're interested in a titillating tale, ask me about a
-#' *former* Spokane County Public Works Department supervisor who had an affair on company
-#' time and wrote emails to his paramour, a supervisee, using his Spokane County
-#' email. And then had his emails requested as part of a public record search by
-#' the local newspaper.
-#'
-#' @format A data.frame with ~124,000 rows and 7 variables:
-#' \describe{
-#'   \item{Name}{Employee name}
-#'   \item{JobTitle}{Job title}
-#'   \item{Sal2013}{Salary in 2013}
-#'   \item{Sal2014}{Salary in 2014}
-#'   \item{Sal2015}{Salary in 2015}
-#'   \item{Sal2016}{Salary in 2016}
-#'   \item{Sal2017}{Salary in 2017}
-#' }
+#' The pre-tax salaries of all UW employees from 2014 to 2018 in US dollars.
+#' (NB: Not all employees worked a full year or worked a full year in a given
+#' position.) Why am I including this in this package? Because it's a great data
+#' set and also because 1. you have a right to know as a taxpayer and 2. if
+#' you're a UW employee (and most of the people who will use this package are),
+#' you should know that this information is part of the public record. Same
+#' thing for your UW email, fyi. If you're interested in a titillating tale, ask
+#' me about a *former* Spokane County Public Works Department supervisor who had
+#' an affair on company time and wrote emails to his paramour, a supervisee,
+#' using his Spokane County email... and then had his emails requested as part of
+#' a public record search by the local newspaper.
+#' @source \url{http://fiscal.wa.gov/Salaries.aspx}
+#' @format A data.frame with 9 variables: \describe{ \item{Agy}{Agency ID
+#'   number} \item{AgyTitle}{Agency title} \item{Name}{Employee name}
+#'   \item{JobTitle}{Job title} \item{Sal2014}{Salary in 2014}
+#'   \item{Sal2015}{Salary in 2015} \item{Sal2016}{Salary in 2016}
+#'   \item{Sal2017}{Salary in 2017} \item{Sal2018}{Salary in 2018}}
 "UWsalaries"
 
 
