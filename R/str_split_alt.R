65db0fe568d7490c24f05e72868d4ed9d955f764
Laura Shireman <shireman@uw.edu>
2020-09-10 16:39:11 -0700

    Fixed glitch that was yielding inconsistent results for str_split_alt, sometimes causing it to *not* return a vector.

--- R/str_split_alt.R
+++ R/str_split_alt.R
@@ -10,10 +10,10 @@
 #' @param string The string of text to be split into pieces
 #' @param pattern The pattern to use for splitting the text. The pattern will
 #'   not be retained in the output unless \code{retainPattern = TRUE}.
-#' @param n The index of the output vector to be returned. If left as \code{NA},
-#'   this will return all items in the vector. If set to a specific number n, it
-#'   will return only the nth item of that vector. If set to "last", it will
-#'   return only the last item of the vector.
+#' @param i The index of the output vector to be returned. If left as \code{NA},
+#'   this will return all items in the vector. If set to a specific number
+#'   \emph{i}, it will return only the \emph{i}th item of that vector. If set to
+#'   "last", it will return only the last item of the vector.
 #' @param retainPattern TRUE or FALSE for whether to retain the pattern that was
 #'   used to separate the input text in the output vector. Warning: This was set
 #'   up for a situation in which the user is only looking to match a single,
@@ -29,8 +29,8 @@
 #' # Applied to a vector of length 1:
 #' MyString <- c("First filename.xlsx Second filename.xlsx")
 #' str_split_alt(MyString, ".xlsx")
-#' str_split_alt(MyString, ".xlsx", 1)
-#' str_split_alt(MyString, ".xlsx", "last")
+#' str_split_alt(MyString, ".xlsx", i = 1)
+#' str_split_alt(MyString, ".xlsx", i = "last")
 #' str_split_alt(MyString, ".xlsx", retainPattern = TRUE)
 #'
 #' # Applied to a longer vector, e.g., a column in a data.frame:
@@ -45,28 +45,27 @@
 #'                             str_c(paste0("MyFile", 1:2, ".xlsx"), collapse = " ")))
 #'
 #' MyDF$ColD <- str_split_alt(MyDF$ColB, " ", "last")
-#' MyDF$ColE <- str_split_alt(MyDF$ColB, " ", 1)
-#' MyDF$ColF <- str_split_alt(MyDF$ColC, " ", 4, retainPattern = TRUE)
+#' MyDF$ColE <- str_split_alt(MyDF$ColB, " ", i = 1)
+#' MyDF$ColF <- str_split_alt(MyDF$ColC, " ", i = 4, retainPattern = TRUE)
 #'
 #'
 str_split_alt <- function(string,
                           pattern,
-                          n = NA,
+                          i = NA,
                           retainPattern = FALSE){
-      library(stringr)
 
       # If the supplied input is a vector of length 1, then this is pretty
       # straightforward.
       if(length(string) == 1){
-            S <- str_split(string, pattern)[[1]]
+            S <- stringr::str_split(string, pattern)[[1]]
             S <- S[-which(S == "")]
-            S <- str_trim(S)
+            S <- stringr::str_trim(S)
 
-            if(complete.cases(n)){
-                  if(n == "last"){
+            if(complete.cases(i)){
+                  if(i == "last"){
                         S <- S[length(S)]
                   } else {
-                        S <- S[n]
+                        S <- S[i]
                   }
             }
 
@@ -79,20 +78,20 @@ str_split_alt <- function(string,
             # data.frame, then apply this sequentially.
 
             mySplit <- function(x){
-                  str_trim(str_split(x, pattern)[[1]])
+                  stringr::str_trim(stringr::str_split(x, pattern)[[1]])
             }
 
-            S <- sapply(string, mySplit)
+            S <- lapply(string, mySplit)
 
-            if(complete.cases(n)){
-                  if(n == "last"){
+            if(complete.cases(i)){
+                  if(i == "last"){
                         IndexToRetain <- sapply(S, length)
                   } else {
-                        IndexToRetain <- rep(n, length(S))
+                        IndexToRetain <- rep(i, length(S))
                   }
 
-                  for(i in 1:length(S)){
-                        S[[i]] <- S[[i]][IndexToRetain[i]]
+                  for(j in 1:length(S)){
+                        S[[j]] <- S[[j]][IndexToRetain[j]]
                   }
 
                   S <- as.character(S)
