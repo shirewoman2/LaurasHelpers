4b51f4d605db9d8b55d8c347b0725b8c85144284
Laura Shireman <shireman@uw.edu>
2020-09-25 09:30:04 -0700

    Clarified description wording. Added example.

--- R/cutNumeric.R
+++ R/cutNumeric.R
@@ -3,15 +3,21 @@
 #' This function cuts a vector of numbers just like the base R function
 #' \code{\link[base]{cut}} except that the output will be a vector of numbers
 #' rather than that weird factor that's the default. The number listed in the
-#' output will be the lower boundary of the bin for the number that was used in
+#' output will be the upper boundary of the bin for the number that was used in
 #' the input.
 #'
 #' @param x A numeric string
 #' @param breaks the desired breaks in that string; these are the same as for
-#'   the base function "cut"
+#'   the base function "cut". The number must be <= the bin into which it is
+#'   sorted and the number must be >= the next-lower bin.
 #'
 #' @return Returns a vector of numeric data
 #' @export
+#'
+#' @examples
+#'
+#' cutNumeric(c(-5, 17, 32, 65, 118), breaks = c(c(0, 15, 30, 60, 120)))
+#'
 #'
 
 cutNumeric <- function(x, breaks){
